ead8a70941f3fce35794b6eab6839cace5219a77
Pratee Khatri <prateekkhatri@lucidmotors.com>
2019-07-08 12:24:29 -0700

    Testing Changes

--- demo-app/src/main.cc
+++ demo-app/src/main.cc
@@ -14,9 +14,13 @@
 #include "config/config.h"
 #include "logging/logging.h"
 #include "primary/aktualizr.h"
+#include "primary/results.h"
 #include "utilities/utils.h"
 
 namespace bpo = boost::program_options;
+
+std::string campaign_id_selected;
+Json::Value custom_manifest;
 
 void check_info_options(const bpo::options_description &description, const bpo::variables_map &vm) {
   if (vm.count("help") != 0) {
@@ -106,6 +110,51 @@ void process_event(const std::shared_ptr<event::BaseEvent> &event) {
   }
 }
 
+int campaign_selection(std::size_t campaign_num_ids)
+{
+  int retVal = 0;
+  std::cout << "Select the Campaign to Accept...\n:";
+
+  while (retVal < 1 || retVal > campaign_num_ids)
+  {
+    std::cin >> retVal;
+    if (retVal < 1 || retVal > campaign_num_ids)
+    {
+      std::cout << "Enter index number from the selection\n";
+    }
+  }
+  return retVal;
+}
+
+
+int campaignSelection(std::vector<campaign::Campaign>& campaign_list, Aktualizr * aktualizr)
+{
+  if (campaign_list.size() < 1)
+  {
+    std::cout << "No Campaigns to Accept...\n";
+  }
+  else
+  {
+    int count = 0;
+    int campaign_index = 0;
+    std::vector<std::string> campaign_id_list;
+    std::cout << "--CAMPAIGNS FOUND--...\n";
+    for (const auto &c : campaign_list)
+    {
+      count++;
+      std::cout << count << ". " << c.name << "\n";
+      std::cout << "--- " << c.id << "\n";
+      std::cout << "--- " << c.description << "\n\n";
+      campaign_id_list.push_back(c.id);
+    }
+
+    campaign_index = campaign_selection(campaign_id_list.size());
+    campaign_id_selected = campaign_id_list[campaign_index - 1];
+    std::cout << "Campaign Selected: " << campaign_id_selected << "\n";
+    (*aktualizr).CampaignControl(campaign_id_selected, campaign::Cmd::Accept);
+  }
+}
+
 int main(int argc, char *argv[]) {
   logger_init();
   logger_set_threshold(boost::log::trivial::info);
@@ -128,30 +177,75 @@ int main(int argc, char *argv[]) {
     aktualizr.Initialize();
 
     std::string buffer;
-    while (std::getline(std::cin, buffer)) {
+    while (std::getline(std::cin, buffer)) 
+    {
       boost::algorithm::to_lower(buffer);
-      if (buffer == "senddevicedata") {
+      if (buffer == "senddevicedata") 
+      {
         aktualizr.SendDeviceData();
         std::cout << "\n\nDevice Data Send Test\n\n";
-      } else if (buffer == "fetchmetadata" || buffer == "fetchmeta" || buffer == "checkupdates" || buffer == "check") {
-        auto fut_result = aktualizr.CheckUpdates();
-      } else if (buffer == "download" || buffer == "startdownload") {
+      }
+      else if (buffer == "fetchmetadata" || buffer == "fetchmeta" || buffer == "checkupdates" || buffer == "check") 
+      {
+        aktualizr.CheckUpdates();
+      } 
+      else if (buffer == "download" || buffer == "startdownload") 
+      {
         aktualizr.Download(current_updates);
-      } else if (buffer == "install" || buffer == "uptaneinstall") {
+      }
+      else if (buffer == "install" || buffer == "uptaneinstall") 
+      {
         aktualizr.Install(current_updates);
         current_updates.clear();
-      } else if (buffer == "campaigncheck") {
-        aktualizr.CampaignCheck();
-      } else if (buffer == "pause") {
+      } 
+      else if (buffer == "campaigncheck") 
+      {
+        auto cc_fut = aktualizr.CampaignCheck();
+      } 
+      else if (buffer == "pause") 
+      {
         aktualizr.Pause();
-      } else if (buffer == "resume") {
+      } 
+      else if (buffer == "resume") 
+      {
         aktualizr.Resume();
-      } else if (buffer == "abort") {
+      } 
+      else if (buffer == "abort") 
+      {
         aktualizr.Abort();
-      } else if (buffer == "lucid") {
+      } 
+      else if (buffer == "lucid") 
+      {
         std::cout << "Lucid Air\n";
-      } else if (!buffer.empty()) {
-        std::cout << "Unknown command.\n";
+      } 
+      else if (buffer == "uptanecycle") 
+      {
+        std::cout << "Running Uptane Cycle...\n";
+        aktualizr.UptaneCycle();
+      }
+      else if (buffer == "campaignaccept") 
+      {
+        std::cout << "Campaign Accepted...\n";
+        std::string campaign_id;
+        result::CampaignCheck camps = aktualizr.CampaignCheck().get();
+        campaignSelection(camps.campaigns, &aktualizr);
+      }
+      else if (buffer == "sendmanifest")
+      {
+        std::string update_status = "Complete";
+        std::string custom_string;
+        custom_manifest["Update Status"] = update_status;
+        std::cout << "Enter Custom Text to Send as Manifest: ";
+
+        std::getline(std::cin, custom_string);
+        custom_manifest["Custom"] = custom_string;
+        std::cout << "Added Custom Message: " << custom_string << "\n";
+
+        aktualizr.SendManifest(custom_manifest);
+      }  
+      else if (!buffer.empty()) 
+      {
+        std::cout << "Unknown command\n";
       } 
     }
     r = EXIT_SUCCESS;
